cdfef87f0e65de18513afea2b69601c13f0b7179
j-press <jpressceo@gmail.com>
2014-05-02 19:01:21 -0500

    Update engine.rb

--- lib/soundmanager-rails/engine.rb
+++ lib/soundmanager-rails/engine.rb
@@ -1,5 +1,5 @@
 module Soundmanager
-  if defined?(::Rails) and ::Rails.version >= "3.1"
+  if defined?(::Rails) and ::Rails.version >= "4.1"
     ENV = ::Rails.env
     module Rails
       class Engine < ::Rails::Engine
